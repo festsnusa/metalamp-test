af746e3d789ca43cedd5b7a02619d1ec5ea3df8c
festsnusa <39244533+festsnusa@users.noreply.github.com>
2022-11-10 21:30:13 +0700

    case for more than 1 slider

--- src/blocks/cards/__image-slider/__image-slider.js
+++ src/blocks/cards/__image-slider/__image-slider.js
@@ -1,32 +1,48 @@
 let slideIndex = 1
-const previousButton = document.querySelector('.slideshow-container__prev')
-const nextButton = document.querySelector('.slideshow-container__next')
+let maxSlides = 4
 
-previousButton.addEventListener('click', plusSlides.bind(null, -1))
-nextButton.addEventListener('click', plusSlides.bind(null, 1))
+let slideshowContainer = document.querySelectorAll(".slideshow-container")
 
-showSlides(slideIndex)
+slideshowContainer.forEach((container, i) => {
 
-function plusSlides(n) {
-  showSlides(slideIndex += n)
-}
+  let containerSlides = container.getElementsByClassName('mySlides')
+  let containerDots = container.getElementsByClassName('dot')
 
-function currentSlide(n) {
-  showSlides(slideIndex = n)
-}
+  Array.from(containerDots).forEach((e, i) => {
+    e.addEventListener('click', currentSlide.bind(null, i))
+  })
 
-function showSlides(n) {
-  let i
-  let slides = document.getElementsByClassName("mySlides")
-  let dots = document.getElementsByClassName("dot")
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"
+  let previousButton = container.getElementsByClassName('slideshow-container__prev')[0]
+  let nextButton = container.getElementsByClassName('slideshow-container__next')[0]
+
+  previousButton.addEventListener('click', plusSlides.bind(null, -1))
+  nextButton.addEventListener('click', plusSlides.bind(null, 1))
+
+  showSlides(slideIndex)
+
+  function showSlides(n) {
+    let i
+  
+    if (n > containerSlides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = containerSlides.length }
+    for (i = 0; i < containerSlides.length; i++) {
+      containerSlides[i].style.display = "none"
+    }
+    for (i = 0; i < containerDots.length; i++) {
+      containerDots[i].className = containerDots[i].className.replace(" active", "")
+    }
+    containerSlides[slideIndex - 1].style.display = "block"
+    containerDots[slideIndex - 1].className += " active"
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "")
+
+  function plusSlides(n) {
+    if (slideIndex == maxSlides && n == 1) return
+    else if (slideIndex == 1 && n == -1) return
+    showSlides(slideIndex += n)
   }
-  slides[slideIndex-1].style.display = "block"
-  dots[slideIndex-1].className += " active"
-}
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n)
+  }
+
+})
