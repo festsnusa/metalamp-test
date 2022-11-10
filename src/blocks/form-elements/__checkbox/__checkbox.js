b724af847237587f5aea05014bfbb1257bc144f2
festsnusa <39244533+festsnusa@users.noreply.github.com>
2022-11-10 19:26:19 +0700

    bug fixes

--- src/blocks/form-elements/__checkbox/__checkbox.js
+++ src/blocks/form-elements/__checkbox/__checkbox.js
@@ -1,8 +1,10 @@
-const checkboxHeaders = document.querySelectorAll('.expandable-checkbox__header')
-const checkboxes = document.querySelectorAll('.expandable-checkbox')
+const checkboxHeaders = document.querySelectorAll('.checkbox__header_expandable')
+const checkboxLists = document.querySelectorAll('.checkbox__list_expandable')
+const checkboxIcons = document.querySelectorAll('.checkbox__icon')
 
 checkboxHeaders.forEach((e, i) => {
   e.addEventListener('click', () => {
-    checkboxes[i].classList.toggle('expandable-checkbox_expanded')
+    checkboxLists[i].classList.toggle('checkbox__list_expandable-expanded')
+    checkboxIcons[i].classList.toggle('checkbox__button_transform')
   })
 })
