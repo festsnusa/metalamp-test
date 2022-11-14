7eddab2bd334cd0022160973fe99cd9e835855ba
festsnusa <39244533+festsnusa@users.noreply.github.com>
2022-11-14 18:01:03 +0700

    bug fixes, added images

--- src/blocks/cards/__image-slider/__image-slider.js
+++ src/blocks/cards/__image-slider/__image-slider.js
@@ -1,12 +1,14 @@
-let slideIndex = 1
-let maxSlides = 4
+let slideIndex = []
+let maxSlides = []
 
 let slideshowContainer = document.querySelectorAll(".slideshow-container")
 
 slideshowContainer.forEach((container, i) => {
 
   let containerSlides = container.getElementsByClassName('mySlides')
-  let containerDots = container.getElementsByClassName('dot')
+  maxSlides.push(containerSlides.length)
+  slideIndex.push(1)
+  let containerDots = container.getElementsByClassName('slideshow-container__dot')
 
   Array.from(containerDots).forEach((e, i) => {
     e.addEventListener('click', currentSlide.bind(null, i))
@@ -15,34 +17,37 @@ slideshowContainer.forEach((container, i) => {
   let previousButton = container.getElementsByClassName('slideshow-container__prev')[0]
   let nextButton = container.getElementsByClassName('slideshow-container__next')[0]
 
-  previousButton.addEventListener('click', plusSlides.bind(null, -1))
-  nextButton.addEventListener('click', plusSlides.bind(null, 1))
+  previousButton.addEventListener('click', plusSlides.bind(null, -1, i))
+  nextButton.addEventListener('click', plusSlides.bind(null, 1, i))
 
-  showSlides(slideIndex)
+  showSlides(slideIndex[i], i)
 
-  function showSlides(n) {
-    let i
+  function showSlides(n, i) {
   
-    if (n > containerSlides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = containerSlides.length }
-    for (i = 0; i < containerSlides.length; i++) {
-      containerSlides[i].style.display = "none"
+    if (n > containerSlides.length) { 
+      slideIndex[i] = 1 
+      // return
     }
-    for (i = 0; i < containerDots.length; i++) {
-      containerDots[i].className = containerDots[i].className.replace(" active", "")
+    if (n < 1) { slideIndex[i] = containerSlides.length }
+
+    for (let j = 0; j < containerSlides.length; j++) {
+      containerSlides[j].style.display = "none"
     }
-    containerSlides[slideIndex - 1].style.display = "block"
-    containerDots[slideIndex - 1].className += " active"
+    for (let j = 0; j < containerDots.length; j++) {
+      containerDots[j].className = containerDots[j].className.replace(" slideshow-container__dot_active", "")
+    }
+    containerSlides[slideIndex[i] - 1].style.display = "block"
+    containerDots[slideIndex[i] - 1].className += " slideshow-container__dot_active"
   }
 
-  function plusSlides(n) {
-    if (slideIndex == maxSlides && n == 1) return
-    else if (slideIndex == 1 && n == -1) return
-    showSlides(slideIndex += n)
+  function plusSlides(n, i) {
+    if (slideIndex[i] == maxSlides[i] && n == 1) return
+    else if (slideIndex[i] == 1 && n == -1) return
+    showSlides(slideIndex[i] += n, i)
   }
   
   function currentSlide(n) {
-    showSlides(slideIndex = n)
+    showSlides(slideIndex[i] = n, i)
   }
 
 })
