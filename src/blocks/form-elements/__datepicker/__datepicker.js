3eba7948bd57f165fbb5de42fa5d1d0bf23b9384
festsnusa <39244533+festsnusa@users.noreply.github.com>
2022-11-17 16:44:00 +0700

    calculateCost() moved to datepicker.js

--- src/blocks/form-elements/__datepicker/__datepicker.js
+++ src/blocks/form-elements/__datepicker/__datepicker.js
@@ -1,4 +1,5 @@
-import { calculateCost } from '../../cards/__booking-form/__booking-form.js'
+// import { calculateCost } from '../../cards/__booking-form/__booking-form.js'
+import moment from "https://cdn.skypack.dev/moment@2.29.4"
 
 let daysOfTheWeek = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"]
 
@@ -166,7 +167,7 @@ class DatePicker {
           }
         }
         if (day.date.getMonth() !== this.now.getMonth()) {
-            day.classes.push("calendar__content__day__another");
+          day.classes.push("calendar__content__day__another");
         }
 
         if (day.date.getMonth() < currentDate.getMonth() && day.date.getYear() == currentDate.getYear()) {
@@ -288,13 +289,13 @@ class DatePicker {
     }
 
     // calculateCost()
-      
+
     const datePickers = document.querySelectorAll('.date-picker')
 
     datePickers.forEach(e => {
       e.classList.remove('date-picker__active')
     })
-    
+
   }
   dateToString(date) {
     let string =
@@ -345,5 +346,64 @@ function hideCalendar() {
   datePickers.forEach(datePicker => {
     datePicker.classList.remove('date-picker__active')
   })
- 
+
+}
+
+function calculateCost() {
+
+  let bookingForm = document.querySelector('.booking-form')
+
+  if (bookingForm == null) {
+    return
+  }
+
+  let inputFrom = bookingForm.getElementsByClassName('date-picker-start')[0]
+  let inputTo = bookingForm.getElementsByClassName('date-picker-end')[0]
+  let multipliedPrice = document.querySelector('.booking-form__multiplied')
+
+  let price = convertToNumber(document.querySelector('.price-section__price').innerText)
+  let multipliedResult = document.querySelector('.booking-form__multiplied-result')
+
+  let total = document.querySelector('.booking-form__result')
+
+  if (inputFrom.value == '' || inputTo.value == '') {
+    multipliedPrice.textContent = `${price.toLocaleString('ru-RU')}₽ x 0 суток`
+    multipliedResult.textContent = `0₽`
+    total.textContent = calculateTotalSection(0)
+    return
+  }
+
+  let firstDate = moment(inputFrom.value, 'D/M/YYYY')
+  let secondDate = moment(inputTo.value, 'D/M/YYYY')
+  let diffDays = secondDate.diff(firstDate, 'days')
+
+  let nightsWord = (diffDays == 1) ? 'сутка' : 'суток'
+
+  multipliedPrice.textContent = `${price.toLocaleString('ru-RU')}₽ x ${diffDays} ${nightsWord}`
+  multipliedResult.textContent = `${price * diffDays.toLocaleString('ru-RU')}₽`
+
+  console.log(multipliedPrice.value)
+
+  total.textContent = calculateTotalSection(price * diffDays)
+
+}
+
+function calculateTotalSection(multipliedResult) {
+
+  if (multipliedResult == 0) return `0₽`
+
+  let serviceAmount = document.querySelectorAll('.booking-form__amount')
+  let result = multipliedResult
+
+  serviceAmount.forEach(e => {
+    console.log(e)
+    result += +e.textContent.match(/\d/g).join('')
+  })
+
+  return `${result.toLocaleString('ru-RU')}₽`
+
+}
+
+function convertToNumber(str) {
+  return +str.match(/\d/g).join('')
 }
